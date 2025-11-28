d3dc5653b120bd0a5047578a5420edc7de316f67
Jon Bramley <brammers@gmail.com>
2025-11-28 15:00:59 +0000

    change localhost to 127.0.0.1

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AppConfig(BaseSettings):
     model_config = SettingsConfigDict()
     python_env: str | None = None
-    host: str = "localhost"
+    host: str = "127.0.0.1"
     port: int = 8086
     log_config: str | None = None
     mongo_uri: str | None = None
